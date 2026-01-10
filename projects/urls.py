22306a4a0bd7a5011499a09413347c288f6c1be2
JR <181124438+Mnr04@users.noreply.github.com>
2026-01-10 18:41:37 +0100

    Correction urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet
+from .views import ProjectViewSet, ContributorViewSet
 
 router = DefaultRouter()
 
